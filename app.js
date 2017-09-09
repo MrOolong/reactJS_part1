88e5a98380915c5345bc5cfef761d1a29c3ac3cf
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-09-09 01:04:18 +0000

    created 2 jsx constants and a multi-line expression

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var myArticle = <article></article>
+
+//Create 2 constants
+const p1 = <p id="large">foo</p>;
+
+const p2 = <p id="small">bar</p>;
+
+//creating a multi-line expression
+var myDiv = (
+  <div>
+    <h1>Hello World</h1>  
+  </div>
+);
